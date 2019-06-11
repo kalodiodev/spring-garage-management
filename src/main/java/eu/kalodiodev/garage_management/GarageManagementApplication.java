25a5b44e5a94e2d5e3bd000323af347345f16fb6
Athanasios Raptodimos <thanosra@gmail.com>
2019-06-12 00:04:23 +0300

    Initial commit

---
+++ src/main/java/eu/kalodiodev/garage_management/GarageManagementApplication.java
@@ -0,0 +1,13 @@
+package eu.kalodiodev.garage_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/kalodiodev/garage_management/GarageManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.kalodiodev.garage_management;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GarageManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
